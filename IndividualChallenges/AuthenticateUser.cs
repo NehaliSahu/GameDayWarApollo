7f405a08a74efb089eb5f75d2181505fbc080498
Nehali Sahu <nehalisahu@outlook.com>
2024-12-05 16:21:48 +0530

    Update AuthenticateUser.cs

--- IndividualChallenges/AuthenticateUser.cs
+++ IndividualChallenges/AuthenticateUser.cs
@@ -1,8 +1,9 @@
 public void AuthenticateUser(string password)
 {
-    if (password == "admin123") // Hardcoded password
+    var hashedPassword = _configuration["AdminSettings:HashedPassword"];
+    if (VerifyPassword(password, hashedPassword))
     {
-        GrantAccess(); // Insecurely grants access
+        GrantAccess();
     }
     else
     {
@@ -13,4 +14,14 @@ public void AuthenticateUser(string password)
 private void GrantAccess()
 {
     Console.WriteLine("Access Granted!");
+}
+
+
+private bool VerifyPassword(string password, string hashedPassword)
+{
+    using (var sha256 = SHA256.Create())
+    {
+        var hashedInputPassword = Convert.ToBase64String(sha256.ComputeHash(Encoding.UTF8.GetBytes(password)));
+        return hashedInputPassword == hashedPassword;
+    }
 }
